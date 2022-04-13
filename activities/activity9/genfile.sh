3a31d6aca4c5e1bbc1b3272d670ec3122c20b9c1
EC2 Default User <ec2-user@ip-172-31-17-215.ec2.internal>
2022-04-13 21:22:37 +0000

    Committing activiy 9

--- activities/activity9/genfile.sh
+++ activities/activity9/genfile.sh
@@ -2,12 +2,17 @@
 #list=$(ls ./)
 filename="newfile.txt"
 files=()
+
+#Extract an array of file names from the current directory
 for i in *
 do
 	files+=($i)
 	#echo "$i"
 done
 
+#Check if file exists or not 
+#If file exists output an error 
+#If file doesn't create a new file 
 if [[ ! "${files[*]}" =~ "$filename" ]]
 then 
 	echo "File doesn't exist. Creating file '$filename'"
