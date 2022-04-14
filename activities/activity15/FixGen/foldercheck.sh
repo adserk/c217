29f5c7aa9e43a35283145aea91d54e1b1a6583f4
EC2 Default User <ec2-user@ip-172-31-17-215.ec2.internal>
2022-04-14 12:36:35 +0000

    Commiting Activity 15

---
+++ activities/activity15/FixGen/foldercheck.sh
@@ -0,0 +1,19 @@
+#CHECK if folder already exist before creating a new folder
+folders=()
+
+#Extract an array of folder names from the current directory
+for i in */
+do
+        folders+=($i)
+done
+
+foldername="logs"
+#If folder exists output an error
+#If folder doesn't create a new file
+if [[ ! "${folders[*]}" =~ "$foldername" ]]
+then
+        echo "Folder doesn't exist. Creating folder '$foldernamei'"
+        mkdir $foldername 
+else
+        echo "Folder $foldername already exist"
+fi
