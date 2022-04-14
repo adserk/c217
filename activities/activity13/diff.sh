8062e33b6fa7d59efb9cfa841874c9ab76744c37
EC2 Default User <ec2-user@ip-172-31-17-215.ec2.internal>
2022-04-14 11:15:41 +0000

    Committing Activity 13

---
+++ activities/activity13/diff.sh
@@ -0,0 +1,53 @@
+f1=()
+f2=()
+
+#Read the files using cat
+file1="file1*"
+lines1=$(cat $file1)
+file2="file2*"
+lines2=$(cat $file2)
+
+#For all elements in "lines1" append it to "f1" array
+for l in $lines1
+do 
+	f1+=($l)
+done
+#For all elements in "lines2" append it to "f2" array
+for l in $lines2
+do 
+	f2+=($l)
+done
+
+echo "File 1: ${f1[*]}"
+echo "File 2: ${f2[*]}"
+
+#Need to extract the differences then compare with each array
+diff=()
+only1=()
+only2=()
+#Check for differences that exist only in 2
+for i in "${f2[@]}"
+do 
+	if [[ ! "${f1[*]}" =~ "$i" ]]
+	then
+		diff+=("$i")
+		only2+=("$i")
+	fi
+done
+#Check for differences that exist only in 1
+for i in "${f1[@]}"
+do
+        if [[ ! "${f2[*]}" =~ "$i" ]]
+        then
+                diff+=("$i")
+		only1+=("$i")
+        fi
+done
+
+echo "All the differences: ${diff[*]}"
+echo -e "Only in file 1:\n$only1"
+echo -e "Only in file 2:\n$only2"
+
+
+
+
