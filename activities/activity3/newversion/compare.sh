434d88a922e1327314718ecda1a3007f391529be
EC2 Default User <ec2-user@ip-172-31-17-215.ec2.internal>
2022-04-14 20:10:30 +0000

    Changes in activity 3

---
+++ activities/activity3/newversion/compare.sh
@@ -0,0 +1,50 @@
+#!/bin/s
+lotterynum=()
+array50=()
+array10=()
+
+FILENAME1="lotterynum*"
+LINES1=$(cat $FILENAME1)
+FILENAME2="array50*"
+LINES2=$(cat $FILENAME2)
+FILENAME3="array10*"
+LINES3=$(cat $FILENAME3)
+
+for l in $LINES1
+do 
+	lotterynum+=($l)
+done
+echo "The Lottery number: ${lotterynum[*]}"
+
+for l in $LINES2
+do  
+	array50+=($l)
+	
+done
+echo "5 picks are: ${array50[*]}"
+
+for l in $LINES3
+do 
+	array10+=($l)
+done
+echo "1 pick is: ${array10[*]}"
+
+#Checks for simililarities between two files:
+lottery50=$(grep -w -f lotterynum* array50*)
+#echo "$lottery50"
+#If lottery10 is greater than zero that means we have a match and therefore won the lottery
+if [[ $lottery50 -gt 0 ]]
+then
+echo "You won! Your match is $lottery50"
+else 
+	echo "You didn't win the lottery"
+fi
+
+#If lottery10 is greater than zero that means we have a match and therefore won the bonus
+lottery10=$(grep -w -f lotterynum* array10*)
+if [[ $lottery10 -gt 0 ]]
+then 
+	echo "You won the bonus! Your match is $lottery10"
+else
+	echo "You didn't win the bonus"
+fi
