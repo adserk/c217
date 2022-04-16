7381a6ff4324274ee673af6bf040c7d8e56cfb9c
EC2 Default User <ec2-user@ip-172-31-17-215.ec2.internal>
2022-04-16 02:38:04 +0000

    Changes in activity 2, rename.sh

--- activities/activity2/rename.sh
+++ activities/activity2/rename.sh
@@ -3,21 +3,27 @@ TODAY="date +%Y%m%d"
 filename="file1_`$TODAY`"
 files=()
 
-for i in *
+for i in files/*
 do
         files+=($i)
 done
 
-#If file doesn't exist, create files and rename with today's date as an extension
-#If file already exist then skip and echo error code
-if [[ ! "${files[*]}" =~ "$filename" ]]
+#If we run this script on another day then it will think the files doesn't exist and append the files inside the folder as well. Thus need a check if there are files in the folder already or not
+for i in "${files[*]}"
+do 
+#even without any files in the 'files' folder, the for loop earlier  will still appends 'files/*' to the 'files' array 
+#thus need to check if 'files' array has the string '/' or not. If not then that means there are files inside the folder because there will be filenames instead of 'files/*' in the 'files' array 
+if [[  "$i" = *"/"* ]] && [[ ! "${files[*]}" =~ "$filename" ]]
 then
 	echo "File doesn't exist. Creating files.'"
-        touch file{1..3}.txt
-	for f in *.txt; do mv -- "$f" "${f%.txt}_`$TODAY`.txt"; done
+	touch files/file{1..3}.txt
+	for f in files/*.txt
+        do 
+	        mv -- "$f" "${f%.txt}_`$TODAY`.txt"
+        done
 
-else 
-	echo "Error: Files already exist"
+else
+	 echo "Error: Files already exist"
 fi
-
+done
 
